40838afa27214275fdd587eb1508e69e73873198
natycalvob <nata.calvob@hotmail.com>
2024-04-11 10:37:06 +0200

    add Error handling and static code

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
+"""
+This module implements a Flask web application for emotion detection.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app :
+#Initiate the flask app:
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_detector():
     ''' This code receives the text from the HTML interface and 
-        runs emotion detection over it using emotion_detection()
+        runs emotion detection over it using emotion_detector()
         function. The output returned shows the emotions and their 
         confidence score for the provided text, as well as the 
         dominant emotion. 
@@ -17,13 +21,14 @@ def sent_detector():
 
     # Format the emotion scores
     emotion_scores_str = ', '.join([
-        f"'{emotion}': {score}" 
-        for emotion, score in response.items() 
+        f"'{emotion}': {score}" for emotion, score in response.items() 
         if emotion != 'dominant_emotion'])
-    
+
     # Get the dominant emotion from the response
     dominant_emotion = response['dominant_emotion']
-    
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again."
     # Print the formatted response
     return (f"For the given statement, the system response is {emotion_scores_str}. "
             f"The dominant emotion is <b>{dominant_emotion}</b>.")
